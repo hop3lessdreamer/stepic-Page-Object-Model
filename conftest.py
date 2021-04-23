00a7f6fa1f509d07d9ed331ac6324b185a011cd3
hop3lessdreamer <glow_life@mail.ru>
2021-04-23 11:24:12 +0300

    v.2.1

--- conftest.py
+++ conftest.py
@@ -9,7 +9,8 @@ def pytest_addoption(parser):
 	тестирование. По умолчанию chrome. Пример:
 	pytest --browser_name=firefox test.py
 	"""
-	parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser: chrome or firefox')
+	parser.addoption('--browser_name', action='store', default='chrome', help='Choose browser: chrome or firefox. Default: chrome')
+	parser.addoption('--language', action='store', default='en', help='Choose language: en or ru. Default: en')
 
 @pytest.fixture
 def browser(request):
@@ -17,12 +18,15 @@ def browser(request):
 	Автоматически подтягивается к тестовым ф-циям, методам, если передать в кач-ве параметра (browser)
 	"""
 	browser_name = request.config.getoption('browser_name')
+	user_language = request.config.getoption('language')
 	browser = None
 	if browser_name == 'chrome':
 		options = Options()
 		options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
 		browser = webdriver.Chrome(options=options)
 	elif browser_name == 'firefox':
+		fp = webdriver.FirefoxProfile()
+		fp.set_preference('intl.accept_languages', user_language)
 		browser = webdriver.Firefox()
 	else: raise pytest.UsageError("--browsername should be chrome or firefox")
 	yield browser
