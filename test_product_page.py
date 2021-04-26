5a7beacf16a772b8437b18ca6d8303c861411e33
hop3lessdreamer <glow_life@mail.ru>
2021-04-26 14:00:57 +0300

    added 3 testcases to test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -23,3 +23,26 @@ def test_guest_can_add_product_to_basket(browser, link):
 	page.solve_quiz_and_get_code()
 	assert page.check_success_mess_product_added(product_name), 'Нет сообщения о том, что товар добавлен'
 	assert page.get_bucket_price() == page.product_price, 'Цена корзины не соответствует цене товара'
+
+
+@pytest.mark.parametrize('link', [pytest.param('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/', marks=pytest.mark.xfail)])
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+	page = ProductPage(browser, link)
+	page.open()
+	page.buy_item()
+	assert page.is_not_element_present(*ProductPageLocators.NOTE_PRODUCT_HAS_BEEN_ADDED_TO_BASKET)
+
+
+@pytest.mark.parametrize('link', ['http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'])
+def test_guest_cant_see_success_message(browser, link):
+	page = ProductPage(browser, link)
+	page.open()
+	assert page.is_not_element_present(*ProductPageLocators.NOTE_PRODUCT_HAS_BEEN_ADDED_TO_BASKET)
+
+
+@pytest.mark.parametrize('link', [pytest.param('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/', marks=pytest.mark.xfail)])
+def test_message_disappeared_after_adding_product_to_basket(browser, link):
+	page = ProductPage(browser, link)
+	page.open()
+	page.buy_item()
+	assert page.is_disappeared(*ProductPageLocators.NOTE_PRODUCT_HAS_BEEN_ADDED_TO_BASKET)
