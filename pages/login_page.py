e22101178521bf3b701f75406835ad4d9acdbd0b
hop3lessdreamer <glow_life@mail.ru>
2021-04-26 14:26:51 +0300

    added 2 testcases to test_product_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,12 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert not self.browser.current_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/', 'Вы не на странице авторизации'
+        assert self.browser.current_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/', 'Вы не на странице авторизации'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert not self.is_element_presented(*LoginPageLocators.LOGIN_FORM), 'Нет формы логина'
+        assert self.is_element_presented(*LoginPageLocators.LOGIN_FORM), 'Нет формы логина'
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert not self.is_element_presented(*LoginPageLocators.REGISTRATION_FORM), 'Нет формы регистрации'
+        assert self.is_element_presented(*LoginPageLocators.REGISTRATION_FORM), 'Нет формы регистрации'
--- test_product_page.py
+++ test_product_page.py
@@ -46,3 +46,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
 	page.open()
 	page.buy_item()
 	assert page.is_disappeared(*ProductPageLocators.NOTE_PRODUCT_HAS_BEEN_ADDED_TO_BASKET)
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.go_to_login_page()
+	assert browser.current_url == 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/', 'Вы не на странице авторизации'
